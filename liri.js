204afa48e39e431f976b17e02440e3788f8e6638
Aida Yrysbekova <aidiko20@gmail.com>
2019-07-22 14:24:56 -0400

    set up all vars

--- liri.js
+++ liri.js
@@ -1,7 +1,13 @@
-js
+
 require("dotenv").config();
-js
+// Seeting up our variables
   var keys = require("./keys.js");
-  js
+  var Spotify = require('node-spotify-api');
   var spotify = new Spotify(keys.spotify);
+  var moment = require ('moment');
+  moment().format();
+  var axios = requiren('axios');
+  var fs = require('fs');
+  var command = process.argv[2];
+  var value = process.argv[3];
   
