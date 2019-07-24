f8e197bb589d987c11126285332f1259a3b79a03
Aida Yrysbekova <aidiko20@gmail.com>
2019-07-23 21:01:36 -0400

    added switch command

--- liri.js
+++ liri.js
@@ -1,13 +1,28 @@
 
 require("dotenv").config();
-// Seeting up our variables
-  var keys = require("./keys.js");
-  var Spotify = require('node-spotify-api');
-  var spotify = new Spotify(keys.spotify);
-  var moment = require ('moment');
-  moment().format();
-  var axios = requiren('axios');
-  var fs = require('fs');
-  var command = process.argv[2];
-  var value = process.argv[3];
-  
+// Setting up our variables
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var moment = require('moment');
+moment().format();
+var axios = require('axios');
+var fs = require('fs');
+var command = process.argv[2];
+var value = process.argv[3];
+
+// Switch command
+switch (command) {
+  case "concert-this":
+    concertThis(value);
+    break;
+  case "spotify-this-song":
+    spotifySong(value);
+    break;
+  case "movie-this":
+    movieThis(value);
+    break;
+  case "do-what-it-says":
+    doThis(value);
+    break;
+};
